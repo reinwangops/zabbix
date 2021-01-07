68a241bb0c4ece784d4dd97b71d100607a2dbf28
reinwangops <76520811+reinwangops@users.noreply.github.com>
2021-01-07 18:13:19 +0800

    Update deploy_tcp_status.sh

--- deploy_tcp_status.sh
+++ deploy_tcp_status.sh
@@ -1,3 +1,4 @@
 cd /etc/zabbix/zabbix_agentd.d
 wget -q /etc/zabbix/zabbix_agentd.d https://raw.githubusercontent.com/reinwangops/zabbix/main/status.conf
 wget -q /etc/zabbix/zabbix_agentd.d https://raw.githubusercontent.com/reinwangops/zabbix/main/tcp_status.sh
+chmod 755 /etc/zabbix/zabbix_agentd.d/tcp_status.sh
